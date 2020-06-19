db9abba5e9c6bd6b085528f477f1695cec426c12
ChangOliver <b06902119@ntu.edu.tw>
2020-06-19 20:24:13 +0800

    script

--- run_master.sh
+++ run_master.sh
@@ -2,8 +2,11 @@
 
 ./compile.sh
 cd user_program
-./master 10 ../sample_input/sample_input_1/target_file_1 ../sample_input/sample_input_1/target_file_2 ../sample_input/sample_input_1/target_file_3 ../sample_input/sample_input_1/target_file_4 ../sample_input/sample_input_1/target_file_5 ../sample_input/sample_input_1/target_file_6 ../sample_input/sample_input_1/target_file_7 ../sample_input/sample_input_1/target_file_8 ../sample_input/sample_input_1/target_file_9 ../sample_input/sample_input_1/target_file_10 fcntl
-./master 1 ../sample_input/sample_input_2/target_file fcntl
-./master 1 ../sample_input/sample_input_3/newdata.txt fcntl
+echo Running sample 1
+./master 10 ../input/sample_input_1/target_file_1 ../input/sample_input_1/target_file_2 ../input/sample_input_1/target_file_3 ../input/sample_input_1/target_file_4 ../input/sample_input_1/target_file_5 ../input/sample_input_1/target_file_6 ../input/sample_input_1/target_file_7 ../input/sample_input_1/target_file_8 ../input/sample_input_1/target_file_9 ../input/sample_input_1/target_file_10 mmap
+echo Running sample 2
+./master 1 ../input/sample_input_2/target_file mmap
+echo Running sample 3
+./master 1 ../input/sample_input_3/newdata.txt mmap
 cd ..
 ./clean.sh
